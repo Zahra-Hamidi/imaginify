21fb232f81d1612a853b2154ee5fd322fb26ea2e
zahra <zahraeng369@gmail.com>
2024-04-03 23:07:10 +0330

    add image form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AddTransformatiosPage = () => {
-  return <div>AddTransformatiosPage</div>;
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id || userId}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 };
 
-export default AddTransformatiosPage;
+export default AddTransformationTypePage;
